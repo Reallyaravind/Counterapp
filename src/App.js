f7f6ca7a6f309286170d01cc95c45f65df11f181
Reallyaravind <aravindsubramanian77@gmail.com>
2023-12-21 23:16:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import {Button, Card, Typography} from '@mui/material';
+import {useRecoilValue, useSetRecoilState, RecoilRoot, atom} from 'recoil';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <RecoilRoot>
+      <div style={{display: "flex", justifyContent:"center"}}>
+        <Card style={{padding: 20,width: 500}}>
+          <Typography variant="h5">
+            Welcome to the counter Game
+          </Typography>
+          <br/>
+          <Buttons/>
+          <CountComponent/>
+        </Card>
+      </div>
+    </RecoilRoot>
+  );
+}
+function Buttons(){
+  return <div style={{display: "flex", justifyContent:"space-between"}}>
+    <Decrease/>
+    <Increase/>
+  </div>
+}
+function Increase() {
+  const setCount = useSetRecoilState(countState);
+  return (
+    <div>
+      <Button variant={"contained"} onClick={() => {
+        setCount(existingCount => existingCount + 1)
+      }}>increase counter</Button>
     </div>
   );
 }
 
+
+function Decrease(){
+  const setCount=useSetRecoilState(countState);
+  return <div>
+    <Button variant={"contained"} onClick={()=>{
+      setCount(existingCount=>existingCount-1)
+    }}>decrease counter</Button>
+  </div>
+}
+
+function CountComponent(){
+  const count=useRecoilValue(countState)
+  return <div>
+    <Typography variant="h5" textAlign={"center"}>
+      {count}
+    </Typography>
+  </div>
+}
 export default App;
+
+const countState=atom({
+key:'countState',
+default:0,
+});
